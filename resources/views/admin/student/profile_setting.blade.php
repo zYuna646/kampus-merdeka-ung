fe86cbd6d71be99a042149f318801dc6f145add4
OnyanPokon <mohamadrafiqd@gmail.com>
2024-06-02 21:11:14 +0800

    fix: udpating userprofile edit

--- resources/views/admin/student/profile_setting.blade.php
+++ resources/views/admin/student/profile_setting.blade.php
@@ -19,93 +19,83 @@
     </ol>
   </div>
 
-  <div class="lg:col-span-4 col-span-12  w-full ">
-    <div class="p-6 bg-white flex gap-4 items-center rounded-xl shadow-sm border border-slate-200">
-      <div class=" relative w-fit">
+  <div class="lg:col-span-4 col-span-12 w-full">
+    <div class="p-6 bg-white grid grid-cols-12 gap-4 items-center rounded-xl shadow-sm border border-slate-200">
+      <div class="relative col-span-3">
         <img src="/images/avatar/placeholder.jpg" alt="" class="w-20 rounded-full object-cover">
         <button
           class="absolute right-0 bottom-0 inline-flex items-center justify-center w-6 h-6 me-2 text-sm font-semibold text-gray-800 bg-white hover:bg-slate-100 shadow-md border border-slate-100 rounded-full dark:bg-gray-700 dark:text-gray-300">
           <i class="fas fa-pencil-alt text-xs"></i>
         </button>
       </div>
-      <div class="flex flex-col ">
-        <p class="font-semibold text-base uppercase">Lorem ipsum dolor sit.</p>
-        <span class="text-xs text-slate-500">Lorem ipsum dolor sit amet consectetur.</span>
+      <div class="flex flex-col col-span-8">
+        @auth
+        <p class="font-semibold text-base uppercase">Mohamad Rafiq Daud</p>
+        <span class="text-xs text-slate-500">Role</span>
+        @endauth
         <span class="text-slate-500 text-sm">Universitas Negeri Gorontalo</span>
       </div>
     </div>
     <div class="w-full bg-white border border-gray-200 rounded-lg shadow mt-4">
       <div class="p-6">
         <div class="grid grid-flow-row divide-y-[1px]">
-
-          {{-- <div class="flex items-center gap-4 font-semibold py-4">
-            <span class=""><i class="fas fa-clipboard-check text-xl"></i></span>
-            <p class="text-sm">Log Book Mingguan</p>
-          </div> --}}
-          <div class="flex items-center gap-4 font-semibold pb-4">
-            <span class=""><i class="fas fa-cog text-base"></i></span>
+          <button class="flex items-center gap-4 font-semibold pb-4 section-button" data-section="profile">
+            <span><i class="fas fa-cog text-base"></i></span>
             <p class="text-sm">Pengaturan Profil</p>
-          </div>
-          <div class="flex items-center gap-4 font-semibold py-4">
-            <span class=""><i class="fas fa-user text-base"></i></span>
+          </button>
+          <button class="flex items-center gap-4 font-semibold py-4 section-button" data-section="account">
+            <span><i class="fas fa-user text-base"></i></span>
             <p class="text-sm">Pengaturan Akun</p>
-          </div>
-          <div class="flex items-center gap-4 font-semibold py-4">
-            <span class=""><i class="fas fa-lock text-base"></i></span>
+          </button>
+          <button class="flex items-center gap-4 font-semibold py-4 section-button" data-section="password">
+            <span><i class="fas fa-lock text-base"></i></span>
             <p class="text-sm">Ganti Sandi</p>
-          </div>
+          </button>
           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
           </form>
-
         </div>
       </div>
     </div>
-
   </div>
 
   <div class="lg:col-span-8 col-span-12 w-full flex flex-col gap-y-4">
-    <div class="bg-white rounded-xl border border-slate-200 shadow-sm p-6 flex flex-col gap-y-2">
+    <div id="profile"
+      class="section-content bg-white rounded-xl border border-slate-200 shadow-sm p-6 flex flex-col gap-y-2 hidden">
       <h3 class="py-4 text-lg font-semibold">Pengaturan Profil</h3>
       <hr>
       <div class="flex flex-col py-2">
         <div class="mb-4">
-          <label for="nama lengkap" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
-            Nama Lengkap
-          </label>
+          <label for="nama lengkap" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">Nama
+            Lengkap</label>
           <input type="text" name="nama lengkap" id="nama lengkap" placeholder="nama lengkap"
-            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs " />
+            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
         </div>
         <div class="mb-4">
-          <label for="persentase" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
-            Jenis Kelamin
-          </label>
+          <label for="persentase" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">Jenis
+            Kelamin</label>
           <select
-            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs ">
+            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs">
             <option value="">L</option>
             <option value="">P</option>
           </select>
         </div>
         <div class="mb-4">
-          <label for="tempat lahir" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
-            Tempat Lahir
-          </label>
+          <label for="tempat lahir" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">Tempat
+            Lahir</label>
           <input type="text" name="tempat lahir" id="tempat lahir" placeholder="tempat lahir"
-            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs " />
+            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
         </div>
         <div class="mb-4">
-          <label for="tanggal lahir" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
-            Tanggal Lahir
-          </label>
+          <label for="tanggal lahir" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">Tanggal
+            Lahir</label>
           <input type="date" name="tanggal lahir" id="tanggal lahir" placeholder="tanggal lahir"
-            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs " />
+            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
         </div>
         <div class="mb-4">
-          <label for="agama" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
-            agama
-          </label>
+          <label for="agama" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">agama</label>
           <input type="text" name="agama" id="agama" placeholder="agama"
-            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs " />
+            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
         </div>
       </div>
       <hr>
@@ -116,23 +106,21 @@
         </button>
       </div>
     </div>
-    <div class="bg-white rounded-xl border border-slate-200 shadow-sm p-6 flex flex-col gap-y-2">
+
+    <div id="account"
+      class="section-content bg-white rounded-xl border border-slate-200 shadow-sm p-6 flex flex-col gap-y-2 hidden">
       <h3 class="py-4 text-lg font-semibold">Pengaturan Akun</h3>
       <hr>
       <div class="flex flex-col py-2">
         <div class="mb-4">
-          <label for="username" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
-            username
-          </label>
+          <label for="username" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">username</label>
           <input type="text" name="username" id="username" placeholder="username"
-            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs " />
+            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
         </div>
         <div class="mb-4">
-          <label for="email" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
-            email
-          </label>
+          <label for="email" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">email</label>
           <input type="email" name="email" id="email" placeholder="email"
-            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs " />
+            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
         </div>
       </div>
       <hr>
@@ -143,31 +131,29 @@
         </button>
       </div>
     </div>
-    <div class="bg-white rounded-xl border border-slate-200 shadow-sm p-6 flex flex-col gap-y-2">
+
+    <div id="password"
+      class="section-content bg-white rounded-xl border border-slate-200 shadow-sm p-6 flex flex-col gap-y-2 hidden">
       <h3 class="py-4 text-lg font-semibold">Ganti Sandi</h3>
       <hr>
       <div class="flex flex-col py-2">
-
         <div class="mb-4">
-          <label for="password saat ini" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
-            password saat ini
-          </label>
+          <label for="password saat ini" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">password
+            saat ini</label>
           <input type="password" name="password saat ini" id="password saat ini" placeholder="password saat ini"
-            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs " />
+            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
         </div>
         <div class="mb-4">
-          <label for="password baru" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
-            password baru
-          </label>
+          <label for="password baru" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">password
+            baru</label>
           <input type="password" name="password baru" id="password baru" placeholder="password baru"
-            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs " />
+            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
         </div>
         <div class="mb-4">
-          <label for="ulangi password" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
-            ulangi password
-          </label>
+          <label for="ulangi password" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">ulangi
+            password</label>
           <input type="password" name="ulangi password" id="ulangi password" placeholder="ulangi password"
-            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs " />
+            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
         </div>
         <div>
           <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
@@ -186,16 +172,29 @@
 
   <script>
     document.addEventListener('DOMContentLoaded', function() {
+      const buttons = document.querySelectorAll('.section-button');
+      const sections = document.querySelectorAll('.section-content');
+
+      buttons.forEach(button => {
+        button.addEventListener('click', function() {
+          const sectionToShow = this.getAttribute('data-section');
+
+          sections.forEach(section => {
+            if (section.id === sectionToShow) {
+              section.classList.remove('hidden');
+            } else {
+              section.classList.add('hidden');
+            }
+          });
+        });
+      });
+
       const passwordFields = document.querySelectorAll('input[type="password"]');
       const checkbox = document.getElementById('vehicle1');
-  
+
       checkbox.addEventListener('change', function() {
-        passwordFields.forEach(function(field) {
-          if (checkbox.checked) {
-            field.type = 'text';
-          } else {
-            field.type = 'password';
-          }
+        passwordFields.forEach(field => {
+          field.type = checkbox.checked ? 'text' : 'password';
         });
       });
     });
