89a5b7ab6567b8d9b3ec1e630415f7a9973fa97a
zYuna646 <anggarangga646@gmail.com>
2024-06-15 17:41:32 +0800

    fix verifikasi

--- app/Imports/VerifikasiImport.php
+++ app/Imports/VerifikasiImport.php
@@ -2,32 +2,91 @@
 
 namespace App\Imports;
 
+use App\Models\DailyLog;
+use App\Models\Lokasi;
+use App\Models\Lowongan;
 use App\Models\Mahasiswa;
 use App\Models\ProgramTransaction;
+use App\Models\WeeklyLog;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Carbon\Carbon;
+
 
 class VerifikasiImport implements ToCollection, WithHeadingRow
 {
     /**
-    * @param Collection $collection
-    */
+     * @param Collection $collection
+     */
     public function collection(Collection $collection)
     {
         foreach ($collection as $value) {
             $mahasiswa = Mahasiswa::where("nim", $value['nim'])->first();
-            $lowongan = Mahasiswa::where('code', $value['kode_lowongan'])->first();
-            if (!$mahasiswa && !$lowongan) {
+            $lowongan = Lowongan::where('code', $value['kode_lowongan'])->first();
+            $lokasi = Lokasi::where('code', $value['kode_lokasi'])->first();
+            if (!$mahasiswa && !$lowongan && !$lokasi) {
                 continue;
             }
 
-            $peserta = ProgramTransaction::where('mahasiswa_id', $mahasiswa->id)->where('lowongan_id', $lowongan->id)->first();
-            if (!$peserta) {
+            $mahasiswa = ProgramTransaction::where('mahasiswa_id', $mahasiswa->id)->where('lowongan_id', $lowongan->id)->first();
+            if (!$mahasiswa) {
                 continue;
             }
 
-            $peserta->status_mahasiswa = true;
+            $mahasiswa->status_mahasiswa = true;
+            $mahasiswa->lokasi_id = $lokasi->id;
+            $program = $mahasiswa;
+
+            if ($program->lowongan->isLogBook) {
+                $startDate = Carbon::parse($program->lowongan->tanggal_mulai); // Tanggal awal
+                $endDate = Carbon::parse($program->lowongan->tanggal_selesai);
+                $st = $startDate->copy(); // Create a separate copy of $startDate for $st
+                $e_d = $startDate->endOfWeek()->copy(); // Create a separate copy for $e_d
+                $wk = WeeklyLog::create([
+                    'program_transaction_id' => $program->id,
+                    'start_date' => $st, // Start date
+                    'end_date' => $e_d, // End date
+                    'desc' => ''
+                ]);
+
+
+                $tmp_date = $startDate->copy()->addWeek()->startOfWeek();
+                while ($tmp_date->lte($endDate)) {
+                    // Buat weekly log untuk minggu ini
+
+                    $tmp_end_week = $tmp_date->copy()->endOfWeek();
+                    if ($tmp_end_week->gte($endDate)) {
+                        $tmp_end_week = $endDate;
+                    }
+
+                    WeeklyLog::create([
+                        'program_transaction_id' => $program->id,
+                        'start_date' => $tmp_date->copy()->startOfWeek(), // Start date
+                        'end_date' => $tmp_end_week, // End date
+                        'desc' => ''
+
+                    ]);
+
+                    // Lanjutkan ke minggu berikutnya
+                    $tmp_date->addWeek();
+                }
+
+                foreach ($program->weeklyLog as $key => $item) {
+                    $startDate = Carbon::parse($item->start_date); // Konversi ke objek Carbon
+                    $endDate = Carbon::parse($item->end_date); // Konversi ke objek Carbon
+                    while ($startDate <= $endDate) {
+                        DailyLog::create([
+                            'program_transaction_id' => $program->id,
+                            'desc' => '',
+                            'date' => $startDate,
+                            'weekly_log_id' => $item->id,
+                        ]);
+
+                        $startDate->addDay(); // Perbaikan sintaks
+                    }
+                }
+            }
         }
     }
 }
