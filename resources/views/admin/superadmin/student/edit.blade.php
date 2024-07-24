b013a0af6dc36a99434ad4cd8ec39aec06f0c351
OnyanPokon <mohamadrafiqd@gmail.com>
2024-07-24 21:37:18 +0800

    feat: adding change password field in master user

--- resources/views/admin/superadmin/student/edit.blade.php
+++ resources/views/admin/superadmin/student/edit.blade.php
@@ -8,7 +8,7 @@
       @csrf
       <div class="mb-4">
         <label for="nim" class="block text-sm font-medium text-gray-700 mb-2">NIM</label>
-        <input type="number" name="nim" id="nim" placeholder="Masukan NIM"
+        <input type="text" name="nim" id="nim" placeholder="Masukan NIM"
           class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs"
           value="{{ $mahasiswa->nim }}">
         @error('nim')
@@ -50,6 +50,27 @@
           @endforeach
         </select>
         @error('studi_id')
+        <div class="invalid-feedback text-red-400">
+          {{ $message }}
+        </div>
+        @enderror
+      </div>
+      <div class="mb-4">
+        <label for="new_pass" class="block text-sm font-medium text-gray-700 mb-2">Password Baru</label>
+        <input type="password" name="new_pass" id="new_pass"
+          class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs">
+        @error('new_pass')
+        <div class="invalid-feedback text-red-400">
+          {{ $message }}
+        </div>
+        @enderror
+      </div>
+      <div class="mb-4">
+        <label for="confirm_new_pass" class="block text-sm font-medium text-gray-700 mb-2">Konfirmasi Password
+          Baru</label>
+        <input type="password" name="confirm_new_pass" id="confirm_new_pass"
+          class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs">
+        @error('confirm_new_pass')
         <div class="invalid-feedback text-red-400">
           {{ $message }}
         </div>
